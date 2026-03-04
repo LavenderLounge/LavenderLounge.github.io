92c7c5af2f2cc1aec2942d36a53e29344349d91f
avereeexyz <211985795+avereeexyz@users.noreply.github.com>
2026-03-04 15:56:42 +0100

    replace dark/light emojis with google material symbols; change gradient for dark mode

--- scripts.js
+++ scripts.js
@@ -4,14 +4,19 @@ const modeToggle = document.getElementById("mode-toggle");
 // Check localStorage for saved mode
 const savedMode = localStorage.getItem("theme");
 
+// the use of innerHTML here should be safe since we dont take user input
+
+// TODO: fix the bug where it only switches after user
+// clicks twice on initial load
+
 // Apply the saved mode if exists
 if (savedMode) {
   document.body.classList.add(savedMode);
-  modeToggle.textContent = savedMode === "light-mode" ? "🌙" : "🌞";
+  modeToggle.innerHTML = savedMode === "light-mode" ? "" : "<img src=\"media/dark.svg\" />";
 } else {
   // Default to light mode if no saved mode
   document.body.classList.add("light-mode");
-  modeToggle.textContent = "🌙";
+  modeToggle.innerHTML = "<img src=\"media/dark.svg\" />";
 }
 
 // Toggle between light and dark mode
@@ -19,12 +24,12 @@ modeToggle.addEventListener("click", () => {
   if (document.body.classList.contains("light-mode")) {
     document.body.classList.remove("light-mode");
     document.body.classList.add("dark-mode");
-    modeToggle.textContent = "🌞";
+    modeToggle.innerHTML = "<img src=\"media/dark.svg\" />";
     localStorage.setItem("theme", "dark-mode"); // Save the preference
   } else {
     document.body.classList.remove("dark-mode");
     document.body.classList.add("light-mode");
-    modeToggle.textContent = "🌙";
+    modeToggle.innerHTML = "<img src=\"media/light.svg\" />";
     localStorage.setItem("theme", "light-mode"); // Save the preference
   }
 });
